f341be69791801fdce37b6e704cb7730ed01d1ef
Konstantin Shyshkin <k.shyshkin@cleverdec.com>
2019-05-01 18:41:55 +0300

    add new class DRV8825_buffered

---
+++ src/DRV8825_buffered.cpp
@@ -0,0 +1,64 @@
+/*
+ * DRV8825 - Stepper Motor Driver Driver
+ * Indexer mode only.
+
+ * Copyright (C)2015 Laurentiu Badea
+ *
+ * This file may be redistributed under the terms of the MIT license.
+ * A copy of this license has been included with this distribution in the file LICENSE.
+ */
+#include "DRV8825_buffered.h"
+
+
+DRV8825_buffered::DRV8825_buffered(short steps, short dir_pin, short step_pin, short enable_pin, uint8_t *buffer)
+:DRV8825(steps, dir_pin, step_pin, enable_pin)
+{
+	this->buffer = buffer;
+}
+
+/*
+ * Initialize pins, calculate timings etc
+ */
+void DRV8825_buffered::begin(float rpm, short microsteps){
+    *(this->buffer) |= 1 << dir_pin; // Dir pin HIGH
+    *(this->buffer) &= ~(1 << step_pin); //Step pin LOW
+
+    if IS_CONNECTED(enable_pin){
+        disable();
+    }
+
+    this->rpm = rpm;
+    setMicrostep(microsteps);
+
+    enable();
+}
+
+/*
+ * Enable/Disable the motor by setting a digital flag
+ */
+void DRV8825_buffered::enable(void){
+    if IS_CONNECTED(enable_pin){
+    	if (enable_active_state == HIGH)
+    		*(this->buffer) |= 1 << enable_pin; // set enable HIGH
+    	else
+    		*(this->buffer) &= ~(1 << enable_pin); // set enable LOW
+    };
+    delayMicros(2);
+}
+
+void DRV8825_buffered::disable(void){
+    if IS_CONNECTED(enable_pin){
+        if (enable_active_state == HIGH)
+    		*(this->buffer) &= ~(1 << enable_pin); // set enable LOW
+    	else
+    		*(this->buffer) |= 1 << enable_pin; // set enable HIGH
+    }
+}
+
+void DRV8825_buffered::stepStart(void){
+	*(this->buffer) |= 1 << step_pin;
+}
+
+void DRV8825_buffered::stepFinish(void){
+	*(this->buffer) &= ~(1 << step_pin);
+}
---
+++ src/DRV8825_buffered.h
@@ -0,0 +1,28 @@
+/*
+ * DRV8825 - Stepper Motor Driver Driver (A4988-compatible)
+ * Indexer mode only.
+ *
+ * Copyright (C)2015 Laurentiu Badea
+ *
+ * This file may be redistributed under the terms of the MIT license.
+ * A copy of this license has been included with this distribution in the file LICENSE.
+ */
+#ifndef DRV8825_BUFFERED_H
+#define DRV8825_BUFFERED_H
+#include <Arduino.h>
+#include "DRV8825.h"
+
+class DRV8825_buffered : public DRV8825 {
+
+private:
+    uint8_t *buffer;
+
+public:
+    DRV8825_buffered(short steps, short dir_pin, short step_pin, short enable_pin, uint8_t *buffer);
+    void begin(float rpm=60, short microsteps=1);
+    void stepStart();
+    void stepFinish();
+    void enable(void) override;
+    void disable(void) override;
+};
+#endif // DRV8825_BUFFERED_H
